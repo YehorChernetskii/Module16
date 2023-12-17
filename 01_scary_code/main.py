329c4a0de6a259b34ba3845b3723ac883bfdd189
Yehor Chernetskii <egorchernetskii@gmail.com>
2023-12-17 14:53:58 +0200

    code_fixed

--- 01_scary_code/main.py
+++ 01_scary_code/main.py
@@ -22,3 +22,19 @@ print(t)
 print(d)
 
 # TODO переписать программу
+
+a = [1, 5, 3]
+b = [1, 5, 1, 5]
+c = [1, 3, 1, 5, 3, 3]
+
+a.extend(b)
+count_5_first = a.count(5)
+print(f"Кол-во цифр 5 при первом объединении: {count_5_first}")
+
+a = [i for i in a if i != 5]
+
+a.extend(c)
+count_3_second = a.count(3)
+print(f"Кол-во цифр 3 при втором объединении: {count_3_second}")
+
+print("Итоговый список:", a)
