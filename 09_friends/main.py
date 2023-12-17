d07eafb8c8fd0b5283f848f0c3ebf59035c2ea4f
Yehor Chernetskii <egorchernetskii@gmail.com>
2023-12-17 17:23:44 +0200

    debt_note_added

--- 09_friends/main.py
+++ 09_friends/main.py
@@ -1,1 +1,28 @@
 # TODO здесь писать код
+
+def calculate_balances(N, K, transactions):
+    balances = {i + 1: 0 for i in range(N)}
+
+    for i in range(K):
+        borrower, lender, amount = transactions[i]
+        balances[borrower] -= amount
+        balances[lender] += amount
+
+    return balances
+
+N = int(input("Кол-во друзей: "))
+K = int(input("Долговых расписок: "))
+
+transactions = []
+for i in range(K):
+    print(f"{i + 1} расписка")
+    borrower = int(input("Кому: "))
+    lender = int(input("От кого: "))
+    amount = int(input("Сколько: "))
+    transactions.append((borrower, lender, amount))
+
+balances = calculate_balances(N, K, transactions)
+
+print("Баланс друзей:")
+for friend, balance in balances.items():
+    print(f"{friend} : {balance}")
