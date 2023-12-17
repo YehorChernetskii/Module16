485870a166bb3d1140768e19125dfdedc5ec0c98
Yehor Chernetskii <egorchernetskii@gmail.com>
2023-12-17 17:04:54 +0200

    last_person_standing_added

--- 08_rhyme_cnt/main.py
+++ 08_rhyme_cnt/main.py
@@ -1,1 +1,21 @@
 # TODO здесь писать код
+
+def last_person_standing(N, K):
+    people = list(range(1, N + 1))
+
+    index = 0
+    while len(people) > 1:
+        index = (index + K - 1) % len(people)
+        removed_person = people.pop(index)
+        print(f"Выбывает человек под номером {removed_person}")
+        print(f"Текущий круг людей: {people}")
+
+    return people[0]
+
+
+N = int(input("Кол-во человек: "))
+K = int(input("Какое число в считалке? "))
+
+result = last_person_standing(N, K)
+
+print(f"\nОстался человек под номером {result}")
