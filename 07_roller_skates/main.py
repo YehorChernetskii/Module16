471daa77323ad684b0bc48c0a3934c4b42879273
Yehor Chernetskii <egorchernetskii@gmail.com>
2023-12-17 16:53:50 +0200

    roller_skates_set_added

--- 07_roller_skates/main.py
+++ 07_roller_skates/main.py
@@ -1,1 +1,22 @@
 # TODO здесь писать код
+
+num_skates = int(input("Кол-во коньков: "))
+skates_sizes = [int(input(f"Размер {i + 1} пары: ")) for i in range(num_skates)]
+
+num_people = int(input("Кол-во людей: "))
+people_foot_sizes = [int(input(f"Размер ноги {i + 1} человека: ")) for i in range(num_people)]
+
+skates_sizes.sort()
+people_foot_sizes.sort()
+
+max_people = 0
+skate_index = 0
+
+for foot_size in people_foot_sizes:
+    while skate_index < num_skates and skates_sizes[skate_index] < foot_size:
+        skate_index += 1
+    if skate_index < num_skates:
+        max_people += 1
+        skate_index += 1
+
+print("Наибольшее кол-во людей, которые могут взять ролики:", max_people)
