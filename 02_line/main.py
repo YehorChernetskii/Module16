459b789cb9a7c0fdfcb518f344a6df5076953db7
Yehor Chernetskii <egorchernetskii@gmail.com>
2023-12-17 15:06:32 +0200

    linecode_added

--- 02_line/main.py
+++ 02_line/main.py
@@ -1,1 +1,11 @@
 # TODO здесь писать код
+
+class1_heights = list(range(160, 177, 2))
+class2_heights = list(range(162, 181, 3))
+
+# Объединение списков и сортировка
+combined_heights = class1_heights + class2_heights
+combined_heights.sort()
+
+# Вывод отсортированного списка
+print("Отсортированный список ростов:", combined_heights)
